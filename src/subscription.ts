11413a3b4973e19cf3fa913c2ee0da2fb8ce7a44
Robert Torgov <rtorgov@monetago.com>
2023-08-02 23:01:57 -0400

    feat: create account filter

--- src/subscription.ts
+++ src/subscription.ts
@@ -12,15 +12,36 @@ export class FirehoseSubscription extends FirehoseSubscriptionBase {
     // This logs the text of every post off the firehose.
     // Just for fun :)
     // Delete before actually using
-    for (const post of ops.posts.creates) {
-      console.log(post.record.text)
-    }
+    // for (const post of ops.posts.creates) {
+    //   console.log(post.author);
+    // }
 
     const postsToDelete = ops.posts.deletes.map((del) => del.uri)
     const postsToCreate = ops.posts.creates
       .filter((create) => {
         // only alf-related posts
-        return create.record.text.toLowerCase().includes('alf')
+        return [
+          'did:plc:7q4nnnxawajbfaq7to5dpbsy',
+          'did:plc:ul3dm3b76hu2cqla3qxb4v6u',
+          'did:plc:nyxviptfyic2lvuorkr5yy3y',
+          'did:plc:k4jt6heuiamymgi46yeuxtpt',
+          'did:plc:uewxgchsjy4kmtu7dcxa77us',
+          'did:plc:eclio37ymobqex2ncko63h4r',
+          'did:plc:k5nskatzhyxersjilvtnz4lh',
+          'did:plc:lia4ywzl2c2kt4dn3kzbywog',
+          'did:plc:k5nskatzhyxersjilvtnz4lh',
+          'did:plc:uzyjut26gy6mcnkf3hfv25sp',
+          'did:plc:ank6pz6hhgvrtmwg7rmmw4fo',
+          'did:plc:yun5yzvbtyt6oge4h35rg2jd',
+          'did:plc:ivseckn32uuldbxyt3qqlsyv',
+          'did:plc:rjtifgmgwnacwa24lqp4tpsy',
+          'did:plc:n2kurkne2vwpccuqwz37ggdm',
+          'did:plc:5o6k7jvowuyaquloafzn3cfw',
+          'did:plc:htnh75yxvugm7c54squy5jrr',
+          'did:plc:7gk35tkrkugkdcqdteviyara',
+          'did:plc:gagt3d5dp2xtw2layuu5uz2s'
+        ].includes(create.author);
+        //
       })
       .map((create) => {
         // map alf-related posts to a db row
