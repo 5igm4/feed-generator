b75a5b6ebf290c96b5c0c915ed9973bd9eda2c18
Robert Torgov <rtorgov@monetago.com>
2023-08-07 20:53:01 -0400

    feat: update subscription logic
    Posts will now be filtered in two ways:
    1. Super accounts will have each post they make included
    2. Selective accounts will only have posts included if a specified
    emoji is used

--- src/subscription.ts
+++ src/subscription.ts
@@ -4,6 +4,31 @@ import {
 } from './lexicon/types/com/atproto/sync/subscribeRepos'
 import { FirehoseSubscriptionBase, getOpsByType } from './util/subscription'
 
+const SUPER_ACCOUNTS = [
+  'did:plc:ul3dm3b76hu2cqla3qxb4v6u',
+  'did:plc:nyxviptfyic2lvuorkr5yy3y',
+  'did:plc:k4jt6heuiamymgi46yeuxtpt',
+  'did:plc:uewxgchsjy4kmtu7dcxa77us',
+  'did:plc:eclio37ymobqex2ncko63h4r',
+  'did:plc:k5nskatzhyxersjilvtnz4lh',
+  'did:plc:lia4ywzl2c2kt4dn3kzbywog',
+  'did:plc:k5nskatzhyxersjilvtnz4lh',
+  'did:plc:uzyjut26gy6mcnkf3hfv25sp',
+  'did:plc:ank6pz6hhgvrtmwg7rmmw4fo',
+  'did:plc:yun5yzvbtyt6oge4h35rg2jd',
+  'did:plc:ivseckn32uuldbxyt3qqlsyv',
+  'did:plc:rjtifgmgwnacwa24lqp4tpsy',
+  'did:plc:n2kurkne2vwpccuqwz37ggdm',
+  'did:plc:5o6k7jvowuyaquloafzn3cfw',
+  'did:plc:htnh75yxvugm7c54squy5jrr',
+  'did:plc:7gk35tkrkugkdcqdteviyara',
+  'did:plc:gagt3d5dp2xtw2layuu5uz2s'
+];
+
+const SELECTIVE_ACCOUNTS = [
+  'did:plc:7q4nnnxawajbfaq7to5dpbsy'
+];
+
 export class FirehoseSubscription extends FirehoseSubscriptionBase {
   async handleEvent(evt: RepoEvent) {
     if (!isCommit(evt)) return
@@ -19,28 +44,8 @@ export class FirehoseSubscription extends FirehoseSubscriptionBase {
     const postsToDelete = ops.posts.deletes.map((del) => del.uri)
     const postsToCreate = ops.posts.creates
       .filter((create) => {
-        // only alf-related posts
-        return [
-          'did:plc:7q4nnnxawajbfaq7to5dpbsy',
-          'did:plc:ul3dm3b76hu2cqla3qxb4v6u',
-          'did:plc:nyxviptfyic2lvuorkr5yy3y',
-          'did:plc:k4jt6heuiamymgi46yeuxtpt',
-          'did:plc:uewxgchsjy4kmtu7dcxa77us',
-          'did:plc:eclio37ymobqex2ncko63h4r',
-          'did:plc:k5nskatzhyxersjilvtnz4lh',
-          'did:plc:lia4ywzl2c2kt4dn3kzbywog',
-          'did:plc:k5nskatzhyxersjilvtnz4lh',
-          'did:plc:uzyjut26gy6mcnkf3hfv25sp',
-          'did:plc:ank6pz6hhgvrtmwg7rmmw4fo',
-          'did:plc:yun5yzvbtyt6oge4h35rg2jd',
-          'did:plc:ivseckn32uuldbxyt3qqlsyv',
-          'did:plc:rjtifgmgwnacwa24lqp4tpsy',
-          'did:plc:n2kurkne2vwpccuqwz37ggdm',
-          'did:plc:5o6k7jvowuyaquloafzn3cfw',
-          'did:plc:htnh75yxvugm7c54squy5jrr',
-          'did:plc:7gk35tkrkugkdcqdteviyara',
-          'did:plc:gagt3d5dp2xtw2layuu5uz2s'
-        ].includes(create.author);
+        // only posts that meet one of our critera
+        return SUPER_ACCOUNTS.includes(create.author) || this.filterPostByAuthorAndEmoji(create.record.text, create.author, '📰', SELECTIVE_ACCOUNTS);
         //
       })
       .map((create) => {
@@ -67,5 +72,9 @@ export class FirehoseSubscription extends FirehoseSubscriptionBase {
         .onConflict((oc) => oc.doNothing())
         .execute()
     }
+  }
+
+  private filterPostByAuthorAndEmoji(text: string, author: string, emoji: string, authors: string[]): boolean {
+    return authors.includes(author) && text.includes(emoji);
   }
 }
