31d20fae324e2eee5d61a5d8fb867393205eee5b
Robert Torgov <rtorgov@monetago.com>
2023-08-07 22:09:16 -0400

    feat: support filtering by phrases

--- src/subscription.ts
+++ src/subscription.ts
@@ -8,42 +8,18 @@ import { FirehoseSubscriptionBase, getOpsByType } from './util/subscription'
 const fetchUsers = process.env.FETCH_USERS?.split(',') || [];
 const result: string[] = [];
 
-
-// const SUPER_ACCOUNTS = [
-//   'did:plc:ul3dm3b76hu2cqla3qxb4v6u',
-//   'did:plc:nyxviptfyic2lvuorkr5yy3y',
-//   'did:plc:k4jt6heuiamymgi46yeuxtpt',
-//   'did:plc:uewxgchsjy4kmtu7dcxa77us',
-//   'did:plc:eclio37ymobqex2ncko63h4r',
-//   'did:plc:k5nskatzhyxersjilvtnz4lh',
-//   'did:plc:lia4ywzl2c2kt4dn3kzbywog',
-//   'did:plc:k5nskatzhyxersjilvtnz4lh',
-//   'did:plc:uzyjut26gy6mcnkf3hfv25sp',
-//   'did:plc:ank6pz6hhgvrtmwg7rmmw4fo',
-//   'did:plc:yun5yzvbtyt6oge4h35rg2jd',
-//   'did:plc:ivseckn32uuldbxyt3qqlsyv',
-//   'did:plc:rjtifgmgwnacwa24lqp4tpsy',
-//   'did:plc:n2kurkne2vwpccuqwz37ggdm',
-//   'did:plc:5o6k7jvowuyaquloafzn3cfw',
-//   'did:plc:htnh75yxvugm7c54squy5jrr',
-//   'did:plc:7gk35tkrkugkdcqdteviyara',
-//   'did:plc:gagt3d5dp2xtw2layuu5uz2s'
-// ];
-
-// const SELECTIVE_ACCOUNTS = [
-//   'did:plc:7q4nnnxawajbfaq7to5dpbsy'
-// ];
-
 export class FirehoseSubscription extends FirehoseSubscriptionBase {
   private SUPER_ACCOUNTS: string[];
   private SELECTIVE_ACCOUNTS: string[];
   private SELECTIVE_EMOJIS: string [];
+  private SELECTIVE_PHRASES: string [];
 
   async loadConfig(): Promise<void> {
     const fetchUsers = process.env.FETCH_USERS?.split(',') || [];
     const selectiveUsers = process.env.FETCH_SELECTIVE_USERS?.split(',') || [];
     
     this.SELECTIVE_EMOJIS = process.env.FETCH_SELECTIVE_EMOJI?.split(',') || [];
+    this.SELECTIVE_PHRASES = process.env.FETCH_SELECTIVE_PHRASES?.split(',') || [];
     this.SUPER_ACCOUNTS = await this.loadAccounts(fetchUsers);
     this.SELECTIVE_ACCOUNTS = await this.loadAccounts(selectiveUsers);
   }
@@ -69,7 +45,7 @@ export class FirehoseSubscription extends FirehoseSubscriptionBase {
     const postsToCreate = ops.posts.creates
       .filter((create) => {
         // only posts that meet one of our critera
-        return this.SUPER_ACCOUNTS.includes(create.author) || this.filterPostByAuthorAndEmojis(create.record.text, create.author, this.SELECTIVE_EMOJIS, this.SELECTIVE_ACCOUNTS);
+        return this.SUPER_ACCOUNTS.includes(create.author) || this.filterPostByAuthorAndEmojis(create.record.text, create.author);
         //
       })
       .map((create) => {
@@ -98,13 +74,22 @@ export class FirehoseSubscription extends FirehoseSubscriptionBase {
     }
   }
 
-  private filterPostByAuthorAndEmojis(text: string, author: string, emojis: string[], authors: string[]): boolean {
-    return authors.includes(author) && this.filterTextForEmojis(text, emojis);
+  private filterPostByAuthorAndEmojis(text: string, author: string): boolean {
+    return this.SELECTIVE_ACCOUNTS.includes(author) && (this.filterTextForEmojis(text, this.SELECTIVE_EMOJIS) || this.filterTextForPhrases(text, this.SELECTIVE_PHRASES));
   }
 
   private filterTextForEmojis(text: string, emojis: string[]): boolean {
     for (const char of text) {
       if (emojis.includes(char)) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  private filterTextForPhrases(text: string, phrases: string[]): boolean {
+    for (const phrase of phrases) {
+      if (text.toLowerCase().includes(phrase)) {
         return true;
       }
     }
