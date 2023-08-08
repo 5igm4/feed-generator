e37cc30d9835103c3b4b508f085b0d2ff584e746
Robert Torgov <rtorgov@monetago.com>
2023-08-08 07:52:15 -0400

    chore: prettier

--- scripts/getDids.ts
+++ scripts/getDids.ts
@@ -1,13 +1,15 @@
-import fetch from 'node-fetch';
+import fetch from 'node-fetch'
 import dotenv from 'dotenv'
 
-(async function main(): Promise<void> {
-    dotenv.config();
-    const fetchUsers = process.env.FETCH_USERS?.split(',') || [];
-    const result: string[] = [];
-    for ( const user of fetchUsers ) {
-        const res = await fetch(`https://bsky.social/xrpc/com.atproto.identity.resolveHandle?handle=${user}`);
-        result.push((await res.json() as any).did);
-    }
-    console.log(result);
-})();
+;(async function main(): Promise<void> {
+  dotenv.config()
+  const fetchUsers = process.env.FETCH_USERS?.split(',') || []
+  const result: string[] = []
+  for (const user of fetchUsers) {
+    const res = await fetch(
+      `https://bsky.social/xrpc/com.atproto.identity.resolveHandle?handle=${user}`,
+    )
+    result.push(((await res.json()) as any).did)
+  }
+  console.log(result)
+})()
--- src/subscription.ts
+++ src/subscription.ts
@@ -1,40 +1,43 @@
-import fetch from 'node-fetch';
+import fetch from 'node-fetch'
 import {
   OutputSchema as RepoEvent,
   isCommit,
 } from './lexicon/types/com/atproto/sync/subscribeRepos'
 import { FirehoseSubscriptionBase, getOpsByType } from './util/subscription'
 
-const fetchUsers = process.env.FETCH_USERS?.split(',') || [];
-const result: string[] = [];
+const fetchUsers = process.env.FETCH_USERS?.split(',') || []
+const result: string[] = []
 
 export class FirehoseSubscription extends FirehoseSubscriptionBase {
-  private SUPER_ACCOUNTS: string[];
-  private SELECTIVE_ACCOUNTS: string[];
-  private SELECTIVE_EMOJIS: string [];
-  private SELECTIVE_PHRASES: string [];
+  private SUPER_ACCOUNTS: string[]
+  private SELECTIVE_ACCOUNTS: string[]
+  private SELECTIVE_EMOJIS: string[]
+  private SELECTIVE_PHRASES: string[]
 
   async loadConfig(): Promise<void> {
-    const fetchUsers = process.env.FETCH_USERS?.split(',') || [];
-    const selectiveUsers = process.env.FETCH_SELECTIVE_USERS?.split(',') || [];
-    
-    this.SELECTIVE_EMOJIS = process.env.FETCH_SELECTIVE_EMOJI?.split(',') || [];
-    this.SELECTIVE_PHRASES = process.env.FETCH_SELECTIVE_PHRASES?.split(',') || [];
-    this.SUPER_ACCOUNTS = await this.loadAccounts(fetchUsers);
-    this.SELECTIVE_ACCOUNTS = await this.loadAccounts(selectiveUsers);
+    const fetchUsers = process.env.FETCH_USERS?.split(',') || []
+    const selectiveUsers = process.env.FETCH_SELECTIVE_USERS?.split(',') || []
+
+    this.SELECTIVE_EMOJIS = process.env.FETCH_SELECTIVE_EMOJI?.split(',') || []
+    this.SELECTIVE_PHRASES =
+      process.env.FETCH_SELECTIVE_PHRASES?.split(',') || []
+    this.SUPER_ACCOUNTS = await this.loadAccounts(fetchUsers)
+    this.SELECTIVE_ACCOUNTS = await this.loadAccounts(selectiveUsers)
   }
 
   async loadAccounts(fetchUsers: string[]): Promise<string[]> {
-    const result: string [] = [];
+    const result: string[] = []
     const promises = fetchUsers.map((user) => {
       return (async () => {
-        const res = await fetch(`https://bsky.social/xrpc/com.atproto.identity.resolveHandle?handle=${user}`);
-        const did = (await res.json() as any).did;
-        result.push(did);
-      })();
-    });
-    await Promise.all(promises);
-    return result;
+        const res = await fetch(
+          `https://bsky.social/xrpc/com.atproto.identity.resolveHandle?handle=${user}`,
+        )
+        const did = ((await res.json()) as any).did
+        result.push(did)
+      })()
+    })
+    await Promise.all(promises)
+    return result
   }
 
   async handleEvent(evt: RepoEvent) {
@@ -45,7 +48,10 @@ export class FirehoseSubscription extends FirehoseSubscriptionBase {
     const postsToCreate = ops.posts.creates
       .filter((create) => {
         // only posts that meet one of our critera
-        return this.SUPER_ACCOUNTS.includes(create.author) || this.filterPostByAuthorAndEmojis(create.record.text, create.author);
+        return (
+          this.SUPER_ACCOUNTS.includes(create.author) ||
+          this.filterPostByAuthorAndEmojis(create.record.text, create.author)
+        )
         //
       })
       .map((create) => {
@@ -75,24 +81,28 @@ export class FirehoseSubscription extends FirehoseSubscriptionBase {
   }
 
   private filterPostByAuthorAndEmojis(text: string, author: string): boolean {
-    return this.SELECTIVE_ACCOUNTS.includes(author) && (this.filterTextForEmojis(text, this.SELECTIVE_EMOJIS) || this.filterTextForPhrases(text, this.SELECTIVE_PHRASES));
+    return (
+      this.SELECTIVE_ACCOUNTS.includes(author) &&
+      (this.filterTextForEmojis(text, this.SELECTIVE_EMOJIS) ||
+        this.filterTextForPhrases(text, this.SELECTIVE_PHRASES))
+    )
   }
 
   private filterTextForEmojis(text: string, emojis: string[]): boolean {
     for (const char of text) {
       if (emojis.includes(char)) {
-        return true;
+        return true
       }
     }
-    return false;
+    return false
   }
 
   private filterTextForPhrases(text: string, phrases: string[]): boolean {
     for (const phrase of phrases) {
       if (text.toLowerCase().includes(phrase)) {
-        return true;
+        return true
       }
     }
-    return false;
+    return false
   }
 }
